4ebc363ab9129087e6b7cb88a6701e8eede2de8f
JosefinaAlancay <AyLin98M@gmail.com>
2025-06-19 21:18:55 -0300

    Se agregó fila de estadísticas.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from "./components/footer/footer";
+import { Stats } from './components/stats/stats';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, Stats],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
