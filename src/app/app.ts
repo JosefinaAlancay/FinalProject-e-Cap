01c8e914a6b088695c84edc22aaff60e755a5269
Sebastián <sebas98ortega@gmail.com>
2025-06-23 15:13:52 -0300

    cambios en el header

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from "./components/footer/footer";
+import { Navbar } from "./components/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
