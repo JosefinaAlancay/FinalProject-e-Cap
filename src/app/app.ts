767fadf278b8c4cf0f18c28c05e3b01bef7847d4
JosefinaAlancay <AyLin98M@gmail.com>
2025-06-25 21:40:28 -0300

    Se agregó la página de Login.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [Footer, RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-
 export class App {
   protected title = 'e-Cap_FrontEnd';
+  showHeaderFooter = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const currentUrl = this.router.url.split('?')[0];
+        console.log('Ruta actual:', currentUrl);
+        this.showHeaderFooter = currentUrl !== '/login';
+      });
+  }
 }
